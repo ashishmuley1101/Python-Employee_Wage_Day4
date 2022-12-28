7366dd62a7690c5a6862c90599d1bdf217d06997
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-28 15:14:03 +0530

    Use Case 2 calculating per day wage for a day.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random
 
 PRESENT = 1
+WORKING_HOURS = 8
+WAGE_PER_HOUR = 20
 
 val = random.randint(0, 1)
 
-# Use Case 1 Employee Present Or Absent.
 if val == PRESENT:
     print("Employee is present.")
+    print("Employee wage for day Rs.", WORKING_HOURS * WAGE_PER_HOUR)  # Use Case 2 Daily Employee Wage.
 
 else:
     print("Employee is absent.")
