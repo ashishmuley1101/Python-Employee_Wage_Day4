b7c388d928309567132ad2959b3975dbb2f1b626
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-28 15:05:48 +0530

    Use Case 1 for checking employee present or absent.

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+PRESENT = 1
+
+val = random.randint(0, 1)
+
+# Use Case 1 Employee Present Or Absent.
+if val == PRESENT:
+    print("Employee is present.")
+
+else:
+    print("Employee is absent.")
