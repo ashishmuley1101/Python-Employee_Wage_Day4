b12ed5d1d88a2218866f0082caed1fd226dce5e8
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-28 15:20:40 +0530

    Use Case 3 calculating part-time employee wage.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import random
 
 PRESENT = 1
+PRESENT_PART_TIME = 2
 WORKING_HOURS = 8
 WAGE_PER_HOUR = 20
 
-val = random.randint(0, 1)
+val = random.randint(0, 2)
 
 if val == PRESENT:
     print("Employee is present.")
     print("Employee wage for day Rs.", WORKING_HOURS * WAGE_PER_HOUR)  # Use Case 2 Daily Employee Wage.
+
+elif val == PRESENT_PART_TIME:
+    print("Employee is present for part time.")
+    print("Employee wage for part time Rs.", WAGE_PER_HOUR * (WORKING_HOURS/2) ) # Use Case 3 Daily Employee Wage.
 
 else:
     print("Employee is absent.")
